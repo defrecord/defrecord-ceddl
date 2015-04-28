1b482d0a5e9f6173ee63a75ad562080e8b573780
Jason Walsh <j@wal.sh>
2015-04-28 09:01:42 -0700

    Simple static test harness

--- src/ceddl/core.cljs
+++ src/ceddl/core.cljs
@@ -3,10 +3,12 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(println "ceddl.core")
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {}))
 
+(def uv js/window.digitalData)
 
+(println uv)
